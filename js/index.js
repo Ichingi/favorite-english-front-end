698232a89e45cd85008267ca9bd71e703c5f915a
DenMitter <57758gfk@gmail.com>
2024-11-17 17:26:58 +0200

    issue-28

--- js/index.js
+++ js/index.js
@@ -58,3 +58,48 @@ function toggleMenu() {
     const menu = document.querySelector('.nav__items');
     menu.classList.toggle('show');
 }
+
+// Modal window
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    const modalContent = modal.querySelector('.modal-content');
+
+    // Встановлюємо стартові значення для відкриття
+    modal.style.display = "flex";
+    document.body.style.overflow = "hidden";  // Заблокувати скрол
+    modalContent.style.animation = 'none'; // Скидаємо попередню анімацію
+    modalContent.offsetHeight; // Перезапускаємо анімацію
+    modalContent.style.animation = 'slideUp 0.5s ease'; // Додаємо анімацію відкриття заново
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    const modalContent = modal.querySelector('.modal-content');
+    
+    // Додаємо анімацію закриття
+    modalContent.style.animation = 'slideOut 0.5s ease'; 
+    
+    // Після завершення анімації, приховуємо модальне вікно
+    setTimeout(() => {
+        modal.style.display = "none";
+        document.body.style.overflow = "";  // Розблокувати скрол
+    }, 230); // Встановлюємо таймер на тривалість анімації
+}
+
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target == modal) {
+            closeModal(modal.id);
+        }
+    });
+}
+
+window.onkeydown = function(event) {
+    if (event.key === 'Escape') {
+        const modals = document.querySelectorAll('.modal'); // Закрити тільки відкрите модальне вікно
+        modals.forEach(modal => {
+            closeModal(modal.id);
+        });
+    }
+};
