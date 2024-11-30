de53709ff49cdeeb83aae05800d55c234f585877
Ichingi <purshegatimoxa@gmail.com>
2024-11-30 22:23:04 +0200

    issue-41

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,11 @@
 const arrow = document.querySelector('.admin-panel__arrow');
 const adminPanel = document.querySelector('.admin-panel');
+const adminMain = document.querySelector('.admin-main');
+
+adminMain.style.marginLeft = '310px';
 
 arrow.addEventListener('click', () => {
-    adminPanel.classList.toggle('active');    
+    const isActive = adminPanel.classList.toggle('active');
+
+    adminMain.style.marginLeft = isActive ? '310px' : '0';
 });
